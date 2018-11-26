10f1f86ec2902ac497f5f2652d859b3b98c61e87
Boshen <boshenc@gmail.com>
2018-11-26 16:22:01 +0800

    add time

--- src/Main.elm
+++ src/Main.elm
@@ -5,25 +5,27 @@ import Browser.Dom exposing (..)
 import Browser.Events exposing (..)
 import Html exposing (Html)
 import Html.Attributes exposing (height, style, width)
-import Math.Matrix4 as Mat4 exposing (Mat4)
 import Math.Vector2 as Vec2 exposing (Vec2, vec2)
 import Task
+import Time exposing (Posix, posixToMillis)
 import WebGL exposing (Mesh, Shader)
 
 
 type alias Model =
     { width : Int
     , height : Int
+    , time : Float
     }
 
 
 type Msg
     = GotViewport Viewport
+    | Tick
 
 
 init : flags -> ( Model, Cmd Msg )
 init _ =
-    ( { width = 0, height = 0 }, Task.perform GotViewport getViewport )
+    ( { width = 0, height = 0, time = 0 }, Task.perform GotViewport getViewport )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -36,6 +38,11 @@ update msg model =
                         | width = round viewport.width
                         , height = round viewport.height
                     }
+
+                Tick ->
+                    { model
+                        | time = model.time + 1
+                    }
     in
     ( m, Cmd.none )
 
@@ -46,7 +53,7 @@ main =
         { init = init
         , view = view
         , update = update
-        , subscriptions = \_ -> Sub.none
+        , subscriptions = \_ -> Time.every 1000 (\_ -> Tick)
         }
 
 
@@ -67,12 +74,14 @@ view model =
 
 type alias Uniforms =
     { u_resolution : Vec2
+    , u_time : Float
     }
 
 
 uniforms : Model -> Uniforms
 uniforms model =
     { u_resolution = vec2 (toFloat model.width) (toFloat model.height)
+    , u_time = model.time
     }
 
 
@@ -118,11 +127,11 @@ fragmentShader =
     [glsl|
         precision mediump float;
         uniform vec2 u_resolution;
+        uniform float u_time;
 
         void main () {
-          vec2 st = gl_FragCoord.xy / u_resolution;
-          float y = st.x;
-          vec3 color = vec3(y);
+          vec2 st = gl_FragCoord.xy / u_resolution.xy;
+          vec3 color = vec3(st.x * abs(sin(u_time)), st.y * abs(cos(u_time)), 1);
           gl_FragColor = vec4(color, 1.0);
         }
     |]
